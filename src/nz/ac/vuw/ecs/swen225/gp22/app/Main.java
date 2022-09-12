cda39f9d1dbfdda1b604f30fd273650738a9b46c
Kevin Zeng <zengkevi@ecs.vuw.ac.nz>
2022-09-12 15:38:00 +1200

    Initial app structure

--- src/nz/ac/vuw/ecs/swen225/gp22/app/Main.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Main.java
@@ -3,9 +3,7 @@ package nz.ac.vuw.ecs.swen225.gp22.app;
 import javax.swing.*;
 
 public class Main {
-
     public static void main(String[]a){
-
+        SwingUtilities.invokeLater(ChapsChallenge::new);
     }
-
 }
