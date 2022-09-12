58f6ade768efe8eead8edfcc5f35dc43290052cf
zhouzhi <zhouzhi@myvuw.ac.nz>
2022-09-13 11:37:30 +1200

    read and create a new xml file

--- src/nz/ac/vuw/ecs/swen225/gp22/persistency/ReadTest.java
+++
@@ -1,9 +0,0 @@
-package nz.ac.vuw.ecs.swen225.gp22.persistency;
-
-//import dom4j
-import org.dom4j.Document;
-import org.dom4j.DocumentException;
-import org.dom4j.io.SAXReader;
-
-public class ReadTest {
-}
