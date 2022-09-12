83eaa33acd35657fc50dbe4cd6a2605c22350ac5
Oliver Silk <silkoliv@myvuw.ac.nz>
2022-09-12 22:12:44 +1200

    Added support for rendering keys in inventory.

--- src/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
@@ -2,6 +2,7 @@ package nz.ac.vuw.ecs.swen225.gp22.renderer;
 
 import javax.swing.*;
 
+
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,18 +13,24 @@ public class Renderer extends JPanel {
     private final int visionDistance = (visionSize-1)/2;
     private final int tileSize = 64;
 
+    private final int inventorySize = 4;
+    List<Key> inventory = new ArrayList<>();
 
     int cameraX = 0;
     int cameraY = 0;
     private Sprite[][] tiles = new Sprite[9][9];   // Tiles which are within sight of the camera
     private List<Actor> actors = new ArrayList<>(); // Actors which are within sight of the camera
 
+    public Renderer() {
+    }
+
     /**
      * Updates the renderer with what to draw.
      */
-    public void update(int camX, int camY, Tile[][] allTiles, List<Actor> allActors) {
+    public void update(int camX, int camY, Tile[][] allTiles, List<Actor> allActors, List<Key> inventory) {
         cameraX = camX;
         cameraY = camY;
+        this.inventory = inventory;
         for (int i=cameraX-visionDistance;i<cameraX + visionDistance+1;i++) {
             for (int j=cameraY-visionDistance;j<cameraY + visionDistance+1;j++) {
                 int row = j-cameraY+visionDistance;
@@ -50,12 +57,13 @@ public class Renderer extends JPanel {
     public void paintComponent(Graphics g) {
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
-        final int left = (getWidth() - visionSize * tileSize) / 2;
-        final int top = (getHeight() - visionSize * tileSize) / 2;
+        final int cellsLeft = (getWidth() - visionSize * tileSize) / 2;
+        final int cellsTop = Math.min((getHeight() - visionSize * tileSize) / 2, 25);
         g.setColor(Color.WHITE);
-        g.drawRect(left-1, top-1, visionSize * tileSize+1, visionSize * tileSize+1);
-        drawTiles(g, left, top);
-        drawActors(g, left, top);
+        g.drawRect(cellsLeft-1, cellsTop-1, visionSize * tileSize+1, visionSize * tileSize+1);
+        drawTiles(g, cellsLeft, cellsTop);
+        drawActors(g, cellsLeft, cellsTop);
+        drawInventory(g, cellsTop);
     }
 
     public void drawTiles(Graphics g, int left, int top) {
@@ -76,6 +84,23 @@ public class Renderer extends JPanel {
         }
     }
 
+    public void drawInventory(Graphics g, int cellsTop) {
+        final int left = (getWidth() - inventorySize * tileSize) / 2;
+        final int top = (getHeight() + (cellsTop + tileSize * visionSize) - tileSize) / 2;
+        for (int i=0;i<inventorySize;i++) {
+            g.drawRect(left + i * tileSize, top, tileSize, tileSize);
+            if (i < inventory.size()) {
+                Key key = inventory.get(i);
+                g.drawImage(key.sprite.image, left + i * tileSize, top, null);
+                if (key.amount > 1) {
+                    g.drawImage(Sprite.UI_TWO.image,
+                            left + i * tileSize + tileSize-Sprite.UI_TWO.image.getWidth(),
+                            top, null);
+                }
+            }
+        }
+    }
+
     public Point worldToScreen(Point point) {
         return new Point((point.x - cameraX + visionDistance) * tileSize, (point.y - cameraY + visionDistance) * tileSize);
     }
@@ -88,5 +113,7 @@ public class Renderer extends JPanel {
         public Point pos;
         public Sprite sprite = Sprite.PLAYER_DOWN;
     }
+
+    record Key(Sprite sprite, int amount) { }
 }
 
