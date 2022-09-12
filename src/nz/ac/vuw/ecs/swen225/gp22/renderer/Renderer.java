8249cfe46ad0fd9a3eb9307c05d274ff54378423
Oliver Silk <silkoliv@myvuw.ac.nz>
2022-09-12 22:36:36 +1200

    Changed renderer to use Entity based drawing method

--- src/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/renderer/Renderer.java
@@ -1,10 +1,9 @@
 package nz.ac.vuw.ecs.swen225.gp22.renderer;
 
 import javax.swing.*;
-
-
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Renderer extends JPanel {
@@ -18,8 +17,8 @@ public class Renderer extends JPanel {
 
     int cameraX = 0;
     int cameraY = 0;
-    private Sprite[][] tiles = new Sprite[9][9];   // Tiles which are within sight of the camera
-    private List<Actor> actors = new ArrayList<>(); // Actors which are within sight of the camera
+    // Entities currently visible
+    private List<Entity> entities = new ArrayList<>();
 
     public Renderer() {
     }
@@ -27,30 +26,20 @@ public class Renderer extends JPanel {
     /**
      * Updates the renderer with what to draw.
      */
-    public void update(int camX, int camY, Tile[][] allTiles, List<Actor> allActors, List<Key> inventory) {
+    public void update(int camX, int camY, List<Entity> allEntities, List<Key> inventory) {
         cameraX = camX;
         cameraY = camY;
         this.inventory = inventory;
-        for (int i=cameraX-visionDistance;i<cameraX + visionDistance+1;i++) {
-            for (int j=cameraY-visionDistance;j<cameraY + visionDistance+1;j++) {
-                int row = j-cameraY+visionDistance;
-                int col = i-cameraX+visionDistance;
-                if (i < 0 || j < 0 || j > allTiles.length || i > allTiles[0].length)  {
-                    tiles[row][col] = null;
-                    continue;
-                }
-                tiles[row][col] = allTiles[j][i].sprite;
-            }
-        }
-        actors.clear();
-        for (Actor actor : allActors) {
-            if (isActorVisible(actor)) actors.add(actor);
-        }
+        entities.clear();
+        entities = allEntities.stream()
+                .filter(this::isEntityVisible)
+                .sorted(Comparator.comparingInt((Entity e) -> e.depth))
+                .toList();
     }
 
-    boolean isActorVisible(Actor actor) {
-        return actor.pos.x >= cameraX - visionDistance && actor.pos.x <= cameraX + visionDistance
-                && actor.pos.y >= cameraY - visionDistance && actor.pos.y <= cameraY + visionDistance;
+    boolean isEntityVisible(Entity entity) {
+        return entity.pos.x >= cameraX - visionDistance && entity.pos.x <= cameraX + visionDistance
+                && entity.pos.y >= cameraY - visionDistance && entity.pos.y <= cameraY + visionDistance;
     }
 
     @Override
@@ -61,25 +50,14 @@ public class Renderer extends JPanel {
         final int cellsTop = Math.min((getHeight() - visionSize * tileSize) / 2, 25);
         g.setColor(Color.WHITE);
         g.drawRect(cellsLeft-1, cellsTop-1, visionSize * tileSize+1, visionSize * tileSize+1);
-        drawTiles(g, cellsLeft, cellsTop);
-        drawActors(g, cellsLeft, cellsTop);
+        drawEntities(g, cellsLeft, cellsTop);
         drawInventory(g, cellsTop);
     }
 
-    public void drawTiles(Graphics g, int left, int top) {
-        for (int i=0;i<visionSize;i++) {
-            for (int j=0;j<visionSize;j++) {
-                Sprite sprite = tiles[j][i];
-                if (sprite == null) continue;
-                g.drawImage(sprite.image, left + i * tileSize, top + j * tileSize, null);
-            }
-        }
-    }
-
-    public void drawActors(Graphics g, int left, int top) {
-        for (Actor actor : actors) {
-            Sprite sprite = actor.sprite;
-            Point screenPos = worldToScreen(actor.pos);
+    public void drawEntities(Graphics g, int left, int top) {
+        for (Entity entity : entities) {
+            Sprite sprite = entity.sprite;
+            Point screenPos = worldToScreen(entity.pos);
             g.drawImage(sprite.image, left + screenPos.x, top + screenPos.y, null);
         }
     }
@@ -105,13 +83,7 @@ public class Renderer extends JPanel {
         return new Point((point.x - cameraX + visionDistance) * tileSize, (point.y - cameraY + visionDistance) * tileSize);
     }
 
-    // temp (until actual classes are completed)
-    public static class Tile {
-        public Sprite sprite = Sprite.FLOOR;
-    }
-    public static class Actor {
-        public Point pos;
-        public Sprite sprite = Sprite.PLAYER_DOWN;
+    public record Entity(Sprite sprite, Point pos, int depth) {
     }
 
     record Key(Sprite sprite, int amount) { }
