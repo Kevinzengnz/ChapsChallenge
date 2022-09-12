b2f28433d01b85b6ffc8b22b12b7a9e7d533d449
Kevin Zeng <zengkevi@ecs.vuw.ac.nz>
2022-09-12 15:38:00 +1200

    Initial app structure

---
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/ChapsChallenge.java
@@ -0,0 +1,11 @@
+package nz.ac.vuw.ecs.swen225.gp22.app;
+
+import javax.swing.*;
+
+public class ChapsChallenge extends JFrame{
+    ChapsChallenge() {
+        assert SwingUtilities.isEventDispatchThread();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    
+    }
+}
