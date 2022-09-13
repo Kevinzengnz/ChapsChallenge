9618558a515e9a515cef4b88a0221aef4ba53fbd
Alicia Robinson <robinsalic2@ecs.vuw.ac.nz>
2022-09-13 23:56:11 +1200

    Basic move method implemented in player class.

--- src/nz/ac/vuw/ecs/swen225/gp22/domain/Player.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/domain/Player.java
@@ -23,7 +23,7 @@ public class Player extends Actor{
         return keys;
     }
     public void move(){
-
+        point.add(direction.arrow);
     }
     @Override
     public Sprite getSprite() {
