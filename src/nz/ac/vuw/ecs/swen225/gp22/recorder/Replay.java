cb94185ffc7609fe3cbc3be0363e377cf4359a85
alookasank <alookasank@myvuw.ac.nz>
2022-09-13 20:25:49 +1200

    Added Replay.java public structure

--- src/nz/ac/vuw/ecs/swen225/gp22/recorder/Replay.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/recorder/Replay.java
@@ -6,5 +6,27 @@ package nz.ac.vuw.ecs.swen225.gp22.recorder;
  * ID: 300565439
  */
 public class Replay {
+    private int tps;
 
+    public void loadReplay(String replayName){
+
+    }
+
+    public void autoPlay(){
+
+    }
+
+    public void autoPause(){
+
+    }
+
+    public void setReplaySpeed(int tps){this.tps=tps;}
+
+    public void nextTick(){
+
+    }
+
+    public void prevTick(){
+
+    }
 }
