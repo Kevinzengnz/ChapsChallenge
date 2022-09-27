06fabef548dd925a37fcb91b19106c724c79e17b
hayden <haydencurtis123@gmail.com>
2022-09-28 01:15:25 +1300

    Added Random Key generator

--- src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
@@ -3,13 +3,16 @@ package test.nz.ac.vuw.ecs.swen225.gp22.fuzz;
 import static org.junit.Assert.assertTrue;
 
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 import javax.swing.SwingUtilities;
 import org.junit.jupiter.api.Test;
 import nz.ac.vuw.ecs.swen225.gp22.app.*;
 import nz.ac.vuw.ecs.swen225.gp22.domain.*;
+import nz.ac.vuw.ecs.swen225.gp22.recorder.GameRecorder;
 
 /**
  * 
@@ -17,6 +20,15 @@ import nz.ac.vuw.ecs.swen225.gp22.domain.*;
  * ID:300586379
  */
 class FuzzTest {
+	
+	List<Integer> genEvents(int size){
+		List<Integer> es = new ArrayList<Integer>();
+		Random r = new Random();
+		for(int i = 0; i<size; i++) {
+			es.add(r.nextInt(50));
+		}
+		return es;
+	}
 	/**
 	 * @author Hayden Curtis
 	 * test1() : fuzz tests for level 1
@@ -24,15 +36,32 @@ class FuzzTest {
 	@Test
 	public void test1() {
 		//Player Movements
+		Random r = new Random();
 		Player p = new Player(new Point(3, 4));
-		List<Runnable> moves = List.of(() -> p.setDirection(Direction.Up));
+		var m = new Model() {
+			@Override
+			public Player player() {return p;}
+			@Override
+			public List<Entity> entities() {return null;}
+			@Override
+			public GameRecorder recorder() {return null;}
+			@Override
+			public void remove(Entity e) {}
+			@Override
+			public void onGameOver() {}
+			@Override
+			public void onNextLevel() {}
+		};
+		List<Integer> events = List.of(KeyEvent.VK_UP);
+//		checkMovement(events, p, new Point(3, 3), m);
+		System.out.println(genEvents(5) + " Helloooo");
 	}
 	
-	void checkMovement(List<KeyEvent> events, Player p, Point point) {
+	void checkMovement(List<Integer> events, Player p, Point point, Model m) {
 		Keys k = new Keys();
-		for(KeyEvent e : events) {
-			k.keyPressed(e);
-			k.keyReleased(e);
+		for(Integer e : events) {
+			k.getActionsPressed().getOrDefault(e,()->{}).run();
+			p.ping(m);
 		}
 		assertTrue("Expected: " + point + ", Actual: " + p.getPoint(), p.getPoint().equals(point));
 	}
