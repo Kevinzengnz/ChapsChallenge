e199cd8d9eaa9afc8c88d10b3fb6f33d3ff745b8
Kevin Zeng <kevinzeng.nz@gmail.com>
2022-09-27 22:37:19 +1300

    Updated controller and model with preliminary saveGame methods

--- src/nz/ac/vuw/ecs/swen225/gp22/app/Model.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Model.java
@@ -2,9 +2,11 @@ package nz.ac.vuw.ecs.swen225.gp22.app;
 
 import nz.ac.vuw.ecs.swen225.gp22.domain.Entity;
 import nz.ac.vuw.ecs.swen225.gp22.domain.Player;
+import nz.ac.vuw.ecs.swen225.gp22.persistency.XmlParser;
 import nz.ac.vuw.ecs.swen225.gp22.recorder.GameRecorder;
 import nz.ac.vuw.ecs.swen225.gp22.renderer.Renderer;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +22,15 @@ public interface Model{
     void remove(Entity e);
     void onGameOver();
     void onNextLevel();
+
+
+    default void saveGame()   {
+        try {
+            XmlParser.saveGame(entities(), "levelOne");
+        } catch(IOException e) {
+            System.out.println("Error saving game");
+        }
+    }
 
     default void ping(Renderer renderer){
         entities().forEach(a -> a.ping(this));
