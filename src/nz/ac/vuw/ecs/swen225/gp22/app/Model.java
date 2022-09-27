d4525fc477ce1603b5525ad5918231fbc6f57f52
Kevinzengnz <kevinzeng.nz@gmail.com>
2022-09-27 17:41:35 +1300

    Updated ChapsChallenge with setPhase to set to level 1, using Phase class and Model class

--- src/nz/ac/vuw/ecs/swen225/gp22/app/Model.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Model.java
@@ -3,7 +3,9 @@ package nz.ac.vuw.ecs.swen225.gp22.app;
 import nz.ac.vuw.ecs.swen225.gp22.domain.Entity;
 import nz.ac.vuw.ecs.swen225.gp22.domain.Player;
 import nz.ac.vuw.ecs.swen225.gp22.domain.Tile;
+import nz.ac.vuw.ecs.swen225.gp22.renderer.Renderer;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,12 +17,12 @@ public interface Model{
     Player player();
     List<Entity> entities();
     void remove(Entity e);
-    Tile[][] tiles();
     void onGameOver();
     void onNextLevel();
 
-    default void ping(){
+    default void ping(Renderer renderer){
         entities().forEach(a -> {});
+        renderer.update(player().getPoint().x(), player().getPoint().y(), entities(), new ArrayList<>());
         var end = false;
         if(end){ onNextLevel(); }
     }
--- src/nz/ac/vuw/ecs/swen225/gp22/app/Phase.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Phase.java
@@ -4,6 +4,7 @@ import nz.ac.vuw.ecs.swen225.gp22.domain.Entity;
 import nz.ac.vuw.ecs.swen225.gp22.domain.Player;
 import nz.ac.vuw.ecs.swen225.gp22.domain.Point;
 import nz.ac.vuw.ecs.swen225.gp22.domain.Tile;
+import nz.ac.vuw.ecs.swen225.gp22.renderer.Renderer;
 import nz.ac.vuw.ecs.swen225.gp22.renderer.Sprite;
 
 import java.util.ArrayList;
@@ -15,23 +16,25 @@ import java.util.stream.IntStream;
  * @author Kevin Zeng
  * ID: 300563468
  */
-record Phase(Model model, Controller controller) {
+record Phase(Model model, Controller controller, Renderer renderer) {
 
     static Phase newLevel(Runnable next, Runnable first, List<Entity> levelEntities) {
+        Renderer renderer = new Renderer();
         Player p = new Player(new Point(3, 4)); //default location of player
         levelEntities.add(p);
-        Tile[][] tiles = new Tile[20][20]; //default square of tiles
+
+        //Adds tiles and walls to entity list
         for (int i=0;i<20;i++) {
             for (int j=0;j<20;j++) {
                 if (i == 0 || j == 0 || i==19 || j == 19) {
-                    tiles[i][j] = new Tile(Sprite.WALL, new Point(i, j));
+                    levelEntities.add(new Tile(Sprite.WALL, new Point(i, j)));
                 }
                 else {
-                    tiles[i][j] = new Tile(Sprite.FLOOR, new Point(i, j));
+                    levelEntities.add(new Tile(Sprite.FLOOR, new Point(i, j)));
                 }
             }
         }
-
+        renderer.update(p.getPoint().x(), p.getPoint().y(), levelEntities, new ArrayList<>());
         var m = new Model() {
             List<Entity> entities = levelEntities;
 
@@ -53,11 +56,6 @@ record Phase(Model model, Controller controller) {
             }
 
             @Override
-            public Tile[][] tiles() {
-                return tiles;
-            }
-
-            @Override
             public void onGameOver() {
                 first.run();
             }
@@ -67,7 +65,7 @@ record Phase(Model model, Controller controller) {
                 next.run();
             }
         };
-        return new Phase(m, new Controller(p));
+        return new Phase(m, new Controller(p),renderer);
     }
 
     static Phase level1(Runnable next, Runnable first) {
