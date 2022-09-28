e9953ea3612782624f0abfdba2af91703bd010d2
Oliver Silk <silkoliv@myvuw.ac.nz>
2022-09-28 18:24:42 +1300

    Added base functionality for playing Sound effects

--- src/nz/ac/vuw/ecs/swen225/gp22/renderer/SoundEffect.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/renderer/SoundEffect.java
@@ -1,11 +1,29 @@
 package nz.ac.vuw.ecs.swen225.gp22.renderer;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+
 public enum SoundEffect {
-    WALK("walk.wav"),
-    PICKUP("pickup.wav"),
-    DEATH("death.wav");
+    COIN("coin"),
+    UNLOCK("unlock"),
+    PORTAL("portal"),
+    KEY("key");
 
-    SoundEffect(String fileName) {
-
+    final Clip clip;
+    final String name;
+    SoundEffect(String name) {
+        this.name = name;
+        String fileName = "Assets/Audio/" + name + ".wav";
+        try {
+            Clip clip = AudioSystem.getClip();
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(fileName));
+            clip.open(inputStream);
+            this.clip = clip;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
