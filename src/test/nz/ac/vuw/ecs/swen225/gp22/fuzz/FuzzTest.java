d7d77904535c6426063be958f1f61b4020aaff8a
hayden <haydencurtis123@gmail.com>
2022-10-07 17:19:04 +1300

    Creating the Levels

--- src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp22/fuzz/FuzzTest.java
@@ -1,18 +1,16 @@
 package test.nz.ac.vuw.ecs.swen225.gp22.fuzz;
 
-import static org.junit.Assert.assertTrue;
-
-import java.awt.event.KeyEvent;
+import java.awt.event.KeyEvent;  
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.stream.IntStream;
 
-import javax.swing.SwingUtilities;
 import org.junit.jupiter.api.Test;
+
 import nz.ac.vuw.ecs.swen225.gp22.app.*;
 import nz.ac.vuw.ecs.swen225.gp22.domain.*;
-import nz.ac.vuw.ecs.swen225.gp22.recorder.GameRecorder;
+import nz.ac.vuw.ecs.swen225.gp22.domain.*;
+import nz.ac.vuw.ecs.swen225.gp22.recorder.*;
 
 /**
  * 
@@ -20,13 +18,12 @@ import nz.ac.vuw.ecs.swen225.gp22.recorder.GameRecorder;
  * ID:300586379
  */
 class FuzzTest {
-	
-	List<Integer> genEvents(int size){
+	private List<Integer> genEvents(int size){
 		List<Integer> es = new ArrayList<Integer>();
 		Random r = new Random();
 		for(int i = 0; i<size; i++) {
 			int next = r.nextInt(50);
-//			if(!listOfKeys.contains(next)) {continue}
+//			if(!listOfKeys.contains(next)) {continue;}
 			es.add(next);
 		}
 		return es;
@@ -38,43 +35,23 @@ class FuzzTest {
 	 */
 	@Test
 	public void test1() {
+		
+		ChapsChallenge cc = new ChapsChallenge();
+		Phase phase = cc.getPhase();
+		Model m = phase.model();
+		PlayerController controller = phase.controller();
+		
 		//Random Key presses
-		List<Integer> events = genEvents(10);
-
-		//Board
-		Player p = new Player(new Point(3,4));
-		Model m = new Model(){
-			@Override
-			public Player player() {return p;}
-			@Override
-			public List<Entity> entities() {return List.of();}
-			@Override
-			public GameRecorder recorder() {return null;}
-			@Override
-			public void remove(Entity e) {}
-			@Override
-			public void onGameOver() {}
-			@Override
-			public void onNextLevel() {}};
-			
-		//Manual Adding Keys
-		Keys k = new Keys();
-		//UP, DOWN, LEFT, RIGHT ARROWS -- move Chap within the maze
-        k.setAction(KeyEvent.VK_UP,() -> { p.setMoving(true); p.setDirection(Direction.Up);},() -> p.setMoving(false));
-        k.setAction(KeyEvent.VK_DOWN,() -> { p.setMoving(true); p.setDirection(Direction.Down);},() ->p.setMoving(false));
-        k.setAction(KeyEvent.VK_LEFT,() -> { p.setMoving(true); p.setDirection(Direction.Left);},() -> p.setMoving(false));
-        k.setAction(KeyEvent.VK_RIGHT,() -> { p.setMoving(true); p.setDirection(Direction.Right);},() ->p.setMoving(false));
-
-        //WASD also moves Chap within the maze
-        k.setAction(KeyEvent.VK_W,() -> { p.setMoving(true); p.setDirection(Direction.Up);},() -> p.setMoving(false));
-        k.setAction(KeyEvent.VK_S,() -> { p.setMoving(true); p.setDirection(Direction.Down);},() -> p.setMoving(false));
-        k.setAction(KeyEvent.VK_A,() -> { p.setMoving(true); p.setDirection(Direction.Left);},() -> p.setMoving(false));
-        k.setAction(KeyEvent.VK_D,() -> { p.setMoving(true); p.setDirection(Direction.Right);},() -> p.setMoving(false));
-        
-        //Testing
-		checkMovement(events, p, k, m);
+		List<Integer> events = genEvents(100);
 	}
 	
+	/**
+	 * Method used to ensure all Key inputs are working
+	 * @param events
+	 * @param p
+	 * @param k
+	 * @param m
+	 */
 	void checkMovement(List<Integer> events, Player p, Keys k, Model m) {
 		for(Integer e : events) {
 			try{
