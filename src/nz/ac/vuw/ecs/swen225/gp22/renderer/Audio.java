ca73e564eb3de17270f97cd28522487d3d58bdfc
Oliver Silk <silkoliv@myvuw.ac.nz>
2022-10-11 17:03:41 +1300

    Added error checking to Audio

--- src/nz/ac/vuw/ecs/swen225/gp22/renderer/Audio.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/renderer/Audio.java
@@ -4,6 +4,7 @@ import javax.sound.sampled.Clip;
 
 public class Audio {
     public static void playSoundEffect(String name) {
+        if (name == null) return;
         Clip clip = getSoundEffect(name).clip;
         clip.setFramePosition(0);
         clip.start();
