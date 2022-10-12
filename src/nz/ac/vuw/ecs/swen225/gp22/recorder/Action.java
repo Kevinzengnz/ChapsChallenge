7a49ccc80c00bec576b29e68d83f981a74e8d22c
alookasank <alookasank@myvuw.ac.nz>
2022-10-12 19:23:58 +1300

    Style update for GameRecorder.java

--- src/nz/ac/vuw/ecs/swen225/gp22/recorder/Action.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/recorder/Action.java
@@ -1,30 +1,26 @@
 package nz.ac.vuw.ecs.swen225.gp22.recorder;
 
 class Action {
-    /**
-     * Direction of movement change.
-     */
-    private final int dir;
-    /**
-     * Game logic clock frame the movement change occurred in.
-     */
-    private final int frame;
+  private final int dir;
+  private final int frame;
 
-    /**
-     * Constructor for action.
-     * @param direction Direction player changed to
-     * @param crntFrame Frame number of direction changed
-     */
-    Action(final int direction, final int crntFrame) {
-        this.dir = direction;
-        this.frame = crntFrame;
-    }
+  /**
+   * Constructor for action.
+   *
+   * @param direction Direction player changed to
+   * @param crntFrame Frame number of direction changed
+   */
+  Action(final int direction, final int crntFrame) {
+    this.dir = direction;
+    this.frame = crntFrame;
+  }
 
-    //GETTERS
-    public int dir() {
-        return this.dir;
-    }
-    public int frame() {
-        return this.frame;
-    }
+  //GETTERS
+  public int dir() {
+    return this.dir;
+  }
+
+  public int frame() {
+    return this.frame;
+  }
 }
