2b55037e2aff45eee1fe8083d70854d683e141e7
Kevin Zeng <zengkevi@ecs.vuw.ac.nz>
2022-10-12 20:30:43 +1300

    Pressing help button pauses game now as well.

--- src/nz/ac/vuw/ecs/swen225/gp22/app/ChapsChallenge.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/ChapsChallenge.java
@@ -265,9 +265,11 @@ public class ChapsChallenge extends JFrame {
     public void showHelp() {
         if (!helpDialogue) {
             currentPhase.renderer().showPopup("Controls:");
+            pauseGame();
             helpDialogue = true;
         } else {
             currentPhase.renderer().hidePopup();
+            unPauseGame();
             helpDialogue = false;
         }
     }
