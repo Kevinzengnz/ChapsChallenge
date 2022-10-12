33949374a967c0eab9b97a6be16e11793c1f87c4
Kevin Zeng <zengkevi@ecs.vuw.ac.nz>
2022-10-12 18:11:46 +1300

    Added some javadoc and fixed checkstyle

--- src/nz/ac/vuw/ecs/swen225/gp22/app/Model.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/app/Model.java
@@ -11,25 +11,52 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 
 /**
- * Model class
- * Stores information on the game state
+ * Model class, which stores information on the game state.
+ *
  * @author Kevin Zeng
  * ID: 300563468
  */
-public interface Model{
+public interface Model {
+    /**
+     * Gets the time left in the game.
+     *
+     * @return time left in timer
+     */
     int timeLeft();
+
+    /**
+     * Decreases timer.
+     */
     void decrementTime();
+
+    /**
+     * Gets the player character of the game.
+     *
+     * @return player character
+     */
     Player player();
+
+    /**
+     * Gets the list of entities in the level.
+     *
+     * @return List of entities
+     */
     List<Entity> entities();
+
     GameRecorder recorder();
+
     void remove(Entity e);
+
     void onGameOver();
+
     void onNextLevel();
+
     long totalTreasures();
 
     /**
-     * Returns the number of Treasures left in the level
-     * This number will change as the player collects treasures in the level
+     * Returns the number of Treasures left in the level.
+     * This number will change as the player collects treasures in the level.
+     *
      * @return number of treasures in the entities list
      */
     default long treasuresLeft() {
@@ -37,15 +64,15 @@ public interface Model{
     }
 
     /**
-     * Saves the current game to an xml file in the format
-     * "saveGame" + timeStamp
+     * Saves the current game to an xml file in the format: "saveGame" + timeStamp.
      */
     default void saveGame()   {
         try {
-            String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
-            XmlParser.saveGame(entities(), "saveGame" +timeStamp);
+            String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss")
+                    .format(new java.util.Date());
+            XmlParser.saveGame(entities(), "saveGame" + timeStamp);
             recorder().endRecording();
-        } catch(IOException e) {
+        } catch (IOException e) {
             System.out.println("Error saving game");
         }
     }
@@ -55,7 +82,7 @@ public interface Model{
      * "Pings" each entity in the model, ie each entity performs
      * whatever their designated action is for each step of the game.
      */
-    default void ping(){
+    default void ping() {
         entities().forEach(a -> a.ping(this));
         recorder().ping(player().getDirection().ordinal(), player().isMoving());
     }
