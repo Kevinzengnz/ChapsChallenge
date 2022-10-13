bdeaecab018bb2b720727b60f0d91a9056cbf564
zhouzhi <zhouzhi@myvuw.ac.nz>
2022-10-13 20:56:32 +1300

    removed main method

--- src/nz/ac/vuw/ecs/swen225/gp22/persistency/XmlParser.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/persistency/XmlParser.java
@@ -183,18 +183,4 @@ public class XmlParser {
         return timeLeft;
     }
 
-    public static void main(String[] args){
-        ServiceLoader<ExtraActor> loader = ServiceLoader.load(ExtraActor.class);
-
-        Iterator<ExtraActor> iterator = loader.iterator();
-
-
-
-        HashMap<String, ExtraActor> services = new HashMap<>();
-        for (ExtraActor service : loader) {
-            System.out.println(service.getClass().getName());
-            services.put(service.getClass().getName(), service);
-        }
-        System.out.println("Services " + services.size());
-    }
 }
