b557074864f4a323272424b026554d6cd03c0324
Oliver Silk <silkoliv@myvuw.ac.nz>
2022-10-13 22:58:52 +1300

    Updated JavaDoc

--- src/nz/ac/vuw/ecs/swen225/gp22/domain/package-info.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/domain/package-info.java
@@ -1,5 +1,5 @@
-package nz.ac.vuw.ecs.swen225.gp22.domain;
 /**
  * This module is responsible for representing and maintaining the current state of the game,
  * such as what types of objects there are and which actions are allowed to change the state of those objects.
  */
+package nz.ac.vuw.ecs.swen225.gp22.domain;
