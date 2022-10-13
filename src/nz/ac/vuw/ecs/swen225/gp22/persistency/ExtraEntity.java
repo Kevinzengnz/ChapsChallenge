5136fc22e783b448a547ece4e39b8c140580a686
zhouzhi <zhouzhi@myvuw.ac.nz>
2022-10-13 15:48:21 +1300

    not sure how this works

---
+++ src/nz/ac/vuw/ecs/swen225/gp22/persistency/ExtraEntity.java
@@ -0,0 +1,10 @@
+package nz.ac.vuw.ecs.swen225.gp22.persistency;
+
+import nz.ac.vuw.ecs.swen225.gp22.domain.Actor;
+import nz.ac.vuw.ecs.swen225.gp22.domain.Entity;
+
+public interface ExtraEntity {
+    Actor createEntity();
+
+
+}
