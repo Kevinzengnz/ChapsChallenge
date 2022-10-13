a3ff9c31d9c39e75c3e2ad58aa5e34ed521838c8
Alicia Robinson <robinsalic2@myvuw.ac.nz>
2022-10-13 19:32:34 +1300

    Code in domain cleaned up. Robot checks for all Entities made.

--- src/nz/ac/vuw/ecs/swen225/gp22/domain/Robot.java
+++ src/nz/ac/vuw/ecs/swen225/gp22/domain/Robot.java
@@ -1,17 +1,37 @@
 package nz.ac.vuw.ecs.swen225.gp22.domain;
 
-import nz.ac.vuw.ecs.swen225.gp22.app.Model;
 
 import java.util.Arrays;
 import java.util.List;
+import nz.ac.vuw.ecs.swen225.gp22.app.Model;
 
-public class Robot extends Actor{
-    List<Direction> directions = Arrays.asList(Direction.Right, Direction.Down, Direction.Left, Direction.Up);
+/**
+ * Represents Robot for level 2 of the game.
+ *
+ * @author Alicia Robinson - 300560663
+ */
+public class Robot extends Actor {
+    /**
+     * Directions robot can face.
+     */
+    List<Direction> directions = Arrays.asList(Direction.Right,
+            Direction.Down, Direction.Left, Direction.Up);
+    /**
+     * Robots possible sprites.
+     */
     List<String> sprites = Arrays.asList("ROBOT_RIGHT", "ROBOT_DOWN", "ROBOT_LEFT", "ROBOT_UP");
+    /**
+     * Index for sprites and directions lists.
+     */
     int movementIndex = 0;
+    /**
+     * Robots sound effect for when player is killed.
+     */
+    public Runnable soundEffect;
 
     /**
-     * Creates a Robot from the given point
+     * Creates a Robot from the given point.
+     *
      * @param point Starting position of Robot
      */
     protected Robot(Point point) {
@@ -24,29 +44,38 @@ public class Robot extends Actor{
         moveValid = true;
         Point newPoint = point.add(direction.arrow);
         m.entities().stream()
-                .filter(entity -> (entity instanceof WallTile)
+                .filter(entity -> (entity instanceof WallTile
+                        || entity instanceof Door || entity instanceof SewageTile)
                         && entity.getPoint().equals(newPoint))
-                .forEach(entity -> entity.doAction(m, this, newPoint));
-        if(!moveValid){
+                .forEach(entity -> entity.checkRobotMove(m, this, newPoint));
+        if (!moveValid) {
             movementIndex += 1;
-            if(movementIndex == 4){
+            if (movementIndex == 4) {
                 movementIndex = 0;
             }
             setDirection(directions.get(movementIndex));
             setSpriteName(sprites.get(movementIndex));
-        } else{
+        } else {
             this.point = newPoint;
         }
     }
 
     @Override
-    public void doAction(Model model, Actor actor, Point point) {
-        if(!this.getPoint().equals(point)){
+    public void doAction(Model model, Player player, Point point) {
+        if (!this.getPoint().equals(point)) {
             throw new IllegalArgumentException("Player point does not equal SewageTile Point");
         }
+        soundEffect.run();
         model.onGameOver();
     }
 
+    @Override
+    public void setSoundEffect(Runnable soundEffect) {
+        if (soundEffect == null) {
+            throw new IllegalArgumentException("Sound Effect is Null");
+        }
+        this.soundEffect = soundEffect;
+    }
 
 
 }
